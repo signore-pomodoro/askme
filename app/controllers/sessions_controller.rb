8439916d880544921c78bde953d2e125d6857eaf
signore-pomodoro <riudo@list.ru>
2021-11-17 14:36:02 +0300

    users + sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  # Создает в объекте session новый факт залогиненности пользователя, если он
+  # правильно сообщил мэйл/пароль
+  def create
+    @user = User.authenticate(params[:email], params[:password])
+
+    if @user.present?
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: 'вы успешно залогинились'
+    else
+      flash.now.alert = 'Неправильный мэйл или пароль'
+      render :new
+    end
+  end
+
+  # Удаляет сессию залогиненного юзера
+  def destroy
+    # Затигаем в сесси значение ключа :user_id
+    session[:user_id] = nil
+
+    # Редиректим пользователя на главную с сообщением
+    redirect_to root_url, notice: 'Вы разлогинились! Приходите еще!'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,63 +1,21 @@
 Rails.application.routes.draw do
-  root to: 'users#index'
+  root 'users#index'
 
-  resources :questions
-  resources :users
+  # Ресурс пользователей (экшен destroy не поддерживается)
+  resources :users, except: [:destroy]
 
-  get 'show' => 'users#show'
+  # Ресурс сессий (только три экшена :new, :create, :destroy)
+  resources :sessions, only: [:new, :create, :destroy]
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # Ресурс вопросов (кроме экшенов :show, :new, :index)
+  resources :questions, except: [:show, :new, :index]
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
+  # Синонимы путей — в дополнение к созданным в ресурсах выше.
   #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  # Для любознательных: синонимы мы добавили, чтобы показать одну вещь и потом
+  # их удалим.
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
